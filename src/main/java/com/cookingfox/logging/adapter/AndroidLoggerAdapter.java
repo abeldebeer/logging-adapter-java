a9a7af0dc3d13a7c85d84114ef0a2093dced8816
Abel de Beer <abel@cookingfox.nl>
2015-09-17 15:43:24 +0200

    Fixes weird formatting in Android log cat when message is empty, by setting a default message: [empty]

--- src/main/java/com/cookingfox/logging/adapter/AndroidLoggerAdapter.java
+++ src/main/java/com/cookingfox/logging/adapter/AndroidLoggerAdapter.java
@@ -9,32 +9,56 @@ import com.cookingfox.logging.api.LoggerAdapter;
 public class AndroidLoggerAdapter implements LoggerAdapter {
 
     //----------------------------------------------------------------------------------------------
+    // PRIVATE CONSTANTS
+    //----------------------------------------------------------------------------------------------
+
+    private static final String MESSAGE_EMPTY = "[empty]";
+
+    //----------------------------------------------------------------------------------------------
     // PUBLIC METHODS
     //----------------------------------------------------------------------------------------------
 
     @Override
     public void debug(Entry entry) {
-        android.util.Log.d(entry.getCaller(), entry.getMessage());
+        android.util.Log.d(entry.getCaller(), getMessage(entry));
     }
 
     @Override
     public void error(Entry entry) {
-        android.util.Log.e(entry.getCaller(), entry.getMessage());
+        android.util.Log.e(entry.getCaller(), getMessage(entry));
     }
 
     @Override
     public void info(Entry entry) {
-        android.util.Log.i(entry.getCaller(), entry.getMessage());
+        android.util.Log.i(entry.getCaller(), getMessage(entry));
     }
 
     @Override
     public void verbose(Entry entry) {
-        android.util.Log.v(entry.getCaller(), entry.getMessage());
+        android.util.Log.v(entry.getCaller(), getMessage(entry));
     }
 
     @Override
     public void warn(Entry entry) {
-        android.util.Log.w(entry.getCaller(), entry.getMessage());
+        android.util.Log.w(entry.getCaller(), getMessage(entry));
+    }
+
+    //----------------------------------------------------------------------------------------------
+    // PRIVATE METHODS
+    //----------------------------------------------------------------------------------------------
+
+    /**
+     * Re-format the log message.
+     */
+    private String getMessage(Entry entry) {
+        String logMessage = entry.getMessage();
+
+        // Android's logcat has some strange behaviour when a log message is empty.
+        if (logMessage.isEmpty()) {
+            logMessage = MESSAGE_EMPTY;
+        }
+
+        return logMessage;
     }
 
 }
